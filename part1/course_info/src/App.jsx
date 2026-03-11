1a57e2f6650978b42de0987711d7236675011168
Leonel Cantero <lecantero@frba.utn.edu.ar>
2026-03-11 17:40:23 -0300

    Ex. 1.3-1.5

--- part1/course_info/src/App.jsx
+++ part1/course_info/src/App.jsx
@@ -3,28 +3,32 @@ import Content from "./Content"
 import Total from "./Total"
 
 function App() {
-  const course = 'Half Stack application development'
 
-  const parts = [
-    {
-      part: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      part: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      part: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts:
+      [
+        {
+          part: 'Fundamentals of React',
+          exercises: 10
+        },
+        {
+          part: 'Using props to pass data',
+          exercises: 7
+        },
+        {
+          part: 'State of a component',
+          exercises: 14
+        }
+      ]
+
+  }
 
   return (
     <>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total/>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </>
   )
 }
--- part1/course_info/src/Total.jsx
+++ part1/course_info/src/Total.jsx
@@ -1,8 +1,9 @@
 const Total = (props) => {
+    let total = props.parts.reduce((sum,part) => sum+part.exercises,0)
     return (
         <>
             <p>
-                This course has a total of {props.exercises} exercises
+                This course has a total of {total} exercises
             </p>
         </>
     )
