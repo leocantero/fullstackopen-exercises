fa7f6dc8db4c39d95e30c93daaf40ab5d13106c8
Leonel Cantero <lecantero@frba.utn.edu.ar>
2026-05-08 14:46:10 -0300

    Ex 2.2

--- part2/course-info/src/components/Course.jsx
+++ part2/course-info/src/components/Course.jsx
@@ -2,10 +2,13 @@ import Header from './Header'
 import Content from './Content'
 
 const Course = ({ course }) => {
+    const totalExercises = course.parts.reduce((sum, part) => sum + part.exercises, 0)
+
     return (
         <>
             <Header name={course.name} />
             <Content parts={course.parts} />
+            <p><b>Total of {totalExercises} exercises</b></p>
         </>
     )
 }
